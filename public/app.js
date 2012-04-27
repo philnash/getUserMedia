ef5b8f8fdc8197033bc174c68cb068252c8eea2c
Phil Nash <philnash@gmail.com>
2012-04-27 01:02:39 +0100

    Added some comments

--- public/app.js
+++ public/app.js
@@ -1,18 +1,39 @@
+// Set up the PhotoBox object and get the relevant elements
 var PhotoBox = function(){
   var _this=this;
   this.video = document.getElementById('video');
   this.canvas = document.getElementById('canvas');
   this.link = document.getElementById('control');
+
+  // function to call when clicking the video link.
   this.clickEvent = function(e){
     e.preventDefault();
     _this.getStream(e);
   }
+  this.link.addEventListener('click', this.clickEvent, false);
 
+  // hide the video and canvas elements initially
   this.video.setAttribute('style', 'display:none;');
   this.canvas.setAttribute('style', 'display:none;');
-  this.link.addEventListener('click', this.clickEvent, false);
 }
 
+// getStream does the negotiotion of the existence of getUserMedia and uses the
+// vendor prefixed version for Chrome
+PhotoBox.prototype.getStream = function(){
+  var _this=this;
+  if(navigator.getUserMedia){
+    navigator.getUserMedia({video: true}, function(stream){_this.success(stream);}, _this.error);
+  }else if(navigator.webkitGetUserMedia){
+    // we save whether it is webkit as this will work differently later too
+    _this.isWebkit = true;
+    navigator.webkitGetUserMedia('video', function(stream){_this.success(stream);}, _this.error);
+  }else{
+    alert("No getUserMedia available");
+  }
+};
+
+// on getting the stream successfully, we set the source of the video element to
+// the stream. In Chrome we have to make an object url out of it.
 PhotoBox.prototype.success = function(stream){
   var _this = this;
   if(!this.isWebkit){
@@ -22,6 +43,8 @@ PhotoBox.prototype.success = function(stream){
   }
 
   this.video.src = source;
+
+  // then we show the video and repurpose the link for taking photos.
   this.video.setAttribute('style', 'display:block;');
   this.link.removeEventListener('click', this.clickEvent, false);
   this.clickEvent = function(e) {
@@ -32,22 +55,14 @@ PhotoBox.prototype.success = function(stream){
   this.link.addEventListener('click', this.clickEvent, false);
 };
 
+// Alert if there is a problem with the stream.
 PhotoBox.prototype.error = function () {
   alert('Something went wrong');
 };
 
-PhotoBox.prototype.getStream = function(){
-  var _this=this;
-  if(navigator.getUserMedia){
-    navigator.getUserMedia({video: true}, function(stream){_this.success(stream);}, _this.error);
-  }else if(navigator.webkitGetUserMedia){
-    _this.isWebkit = true;
-    navigator.webkitGetUserMedia('video', function(stream){_this.success(stream);}, _this.error);
-  }else{
-    alert("No getUserMedia available");
-  }
-};
 
+// To take a photo, all we do is get the canvas element, clear the context and
+// draw the image straight from the video.
 PhotoBox.prototype.takePhoto = function(){
   var context = this.canvas.getContext('2d');
 
@@ -57,6 +72,4 @@ PhotoBox.prototype.takePhoto = function(){
   this.video.setAttribute('style', 'display:none;');
 };
 
-window.addEventListener('load', function(){
-  new PhotoBox();
-});
+window.addEventListener('load', function(){ new PhotoBox(); });
